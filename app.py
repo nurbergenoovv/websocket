be69d824701ceed4f41b0406dda5e2a408e521c1
Ибрахим Нурберген <ibrahimffyt@gmail.com>
2024-05-17 00:06:48 +0500

    first commit files

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
