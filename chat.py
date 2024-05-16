9fc31c6cdf24b433084286e0e47894ecce505f78
Ибрахим Нурберген <ibrahimffyt@gmail.com>
2024-05-17 01:20:39 +0500

    asda

--- chat.py
+++ chat.py
@@ -7,21 +7,9 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-device_status = 0
-
-@app.route('/')
-def index():
-    return render_template('index.html', status=device_status)
-
 @socketio.on('message')
 def handle_message(msg):
-    global device_status
     print('Received message:'+ msg)
-    if msg == 'toggle':
-        device_status = 1 if device_status == 0 else 0
-        send(device_status, broadcast=True)
-    else:
-        send(device_status, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
