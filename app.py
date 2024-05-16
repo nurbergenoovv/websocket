e12ef853085ae7ecd3c218749b6ea38bd9fdae6c
Ибрахим Нурберген <ibrahimffyt@gmail.com>
2024-05-17 00:21:25 +0500

    asd

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 CORS(app, resources={r"/*": {"origins": "*"}})
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
